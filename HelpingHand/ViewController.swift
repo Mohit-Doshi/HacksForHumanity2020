2d2d67c8763681d1579515074c7d4c6137de0227
Mohit Doshi <mohit.dosi@gmail.com>
2020-10-11 00:23:10 -0700

    initialized iOS App

---
+++ HelpingHand/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelpingHand
+//
+//  Created by Mohit on 10/11/20.
+//  Copyright © 2020 MoDo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
